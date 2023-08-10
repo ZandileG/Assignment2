44f3b13ab0166773d2a27762f7a4502848698411
Zandile Gebuza <2562617@students.wits.ac.za>
2023-08-11 00:08:36 +0200

    Implements Assets in Scenes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+  //This activates the "Play"button's functionality
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+    
+  //The game will end when the "Quit" button is pressed
+    public void Quit()
+    { 
+        Application.Quit();
+    } 
+}

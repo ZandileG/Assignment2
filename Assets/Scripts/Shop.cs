fb396c1a4241457832440846619c2042870e2a6a
Zandile Gebuza <2562617@students.wits.ac.za>
2023-08-18 22:39:39 +0200

    Final Edits

--- Assets/Scripts/Shop.cs
+++ Assets/Scripts/Shop.cs
@@ -4,13 +4,6 @@ using UnityEngine;
 
 public class Shop : MonoBehaviour
 {
-    public List<GameObject> ActualPuzzles = new List<GameObject>();
-
-    public void DeactivatePuzzles(int panelNumber)
-    {
-        ActualPuzzles[panelNumber].SetActive(false);
-    }
-
     //When the player wins a puzzle, they must gain gems maybe 1-5 depending on the level of difficulty
     //The player can use those gems to buy hints
     //The number of gems earned must decrease when the play presses the buy button
